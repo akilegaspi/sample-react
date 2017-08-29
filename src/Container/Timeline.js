5978deb1a2b782a154d58edc0c2947ba1b7680ff
Adrian Legaspi <aki@zap.com.ph>
2017-08-29 20:24:00 +0800

    Create timeline class

---
+++ src/Container/Timeline.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from '../Components/Post';
+
+export default class Timeline extends React.Component {
+
+  constructor(props){
+        this.state = ({
+          user: props.user
+        });
+  }
+
+  render(){
+  }
+
+}
